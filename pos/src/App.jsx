d59a4b200f435727f9a7f842c71c1c29d4e685b0
Noctou <noctouu@gmail.com>
2025-08-09 22:15:56 +0800

    added header

--- pos/src/App.jsx
+++ pos/src/App.jsx
@@ -3,24 +3,29 @@ import useTheme from './hooks/useThemeToggle';
 import ThemeToggle from "./components/ThemeToggle";
 import Sidebar from './components/Sidebar';
 import Orders from './components/Orders';
+import MainContent from './components/MainContent';
 
 import './App.css'
-import MainContent from './components/MainContent';
 
 function App() {
   const [darkMode, setDarkMode] = useTheme();
 
   return (
-      <div className="body">
+      <div className="container">
         <ThemeToggle darkMode={darkMode} toggleTheme={() => setDarkMode(!darkMode)} />
         
         <Sidebar />
           
-        <div className="container">
+        <div className="layer">
           <div className="main-content-container">
             <MainContent />
           </div>
+
           <div className="orders-container">
+            <div className="orders-header">
+              <h1>Orders:</h1>
+            </div>
+
             <Orders />
           </div>
         </div>
--- pos/src/components/Sidebar.jsx
+++ pos/src/components/Sidebar.jsx
@@ -2,15 +2,45 @@ export default function Sidebar() {
     return (
         <div className="side-bar">
             <div className="icon-wrapper-sidebar">
-                <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    height="24px" 
-                    viewBox="0 -960 960 960" 
-                    width="24px" 
-                    fill="#e3e3e3">
-                    <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
+                <div className="icon-tooltip">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        height="24px" 
+                        viewBox="0 -960 960 960" 
+                        width="24px" 
+                        fill="#e3e3e3">
+                        <path d="M221-120q-27 0-48-16.5T144-179L42-549q-5-19 6.5-35T80-600h190l176-262q5-8 14-13t19-5q10 0 19 5t14 13l176 262h192q20 0 31.5 16t6.5 35L816-179q-8 26-29 42.5T739-120H221Zm-1-80h520l88-320H132l88 320Zm260-80q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM367-600h225L479-768 367-600Zm113 240Z"/>
+                    </svg>
+
+                    <span className="tooltip-text">Home</span>
+                </div>
+                
+                <div className="icon-tooltip">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        height="24px" 
+                        viewBox="0 -960 960 960" 
+                        width="24px" 
+                        fill="#e3e3e3">
+                        <path d="M200-80q-33 0-56.5-23.5T120-160v-451q-18-11-29-28.5T80-680v-120q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v120q0 23-11 40.5T840-611v451q0 33-23.5 56.5T760-80H200Zm0-520v440h560v-440H200Zm-40-80h640v-120H160v120Zm200 280h240v-80H360v80Zm120 20Z"/>
+                    </svg>
+
+                    <span className="tooltip-text">Inventory</span>
+                </div>
+
+                <div className="icon-tooltip">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        height="24px" 
+                        viewBox="0 -960 960 960" 
+                        width="24px" 
+                        fill="#e3e3e3">
+                        <path d="M480-120q-138 0-240.5-91.5T122-440h82q14 104 92.5 172T480-200q117 0 198.5-81.5T760-480q0-117-81.5-198.5T480-760q-69 0-129 32t-101 88h110v80H120v-240h80v94q51-64 124.5-99T480-840q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480q0 75-28.5 140.5t-77 114q-48.5 48.5-114 77T480-120Zm112-192L440-464v-216h80v184l128 128-56 56Z"/>
+                    </svg>
+
+                    <span className="tooltip-text">History</span>
+                </div>
             </div>
         </div>
-        
     );
 }
