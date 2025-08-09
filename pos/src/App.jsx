6228d942f7fb255239d1bd348cad89c97253b4ce
Qutiepatutie <ralphandre8@gmail.com>
2025-08-09 20:01:54 +0800

    adjusted container positions

--- pos/src/App.jsx
+++ pos/src/App.jsx
@@ -5,21 +5,24 @@ import Sidebar from './components/Sidebar';
 import Orders from './components/Orders';
 
 import './App.css'
+import MainContent from './components/MainContent';
 
 function App() {
   const [darkMode, setDarkMode] = useTheme();
 
   return (
-      <div className="container">
+      <div className="body">
         <ThemeToggle darkMode={darkMode} toggleTheme={() => setDarkMode(!darkMode)} />
         
         <Sidebar />
           
-        <div className="layer">
+        <div className="container">
+          <div className="main-content-container">
+            <MainContent />
+          </div>
           <div className="orders-container">
             <Orders />
           </div>
-
         </div>
       </div>
   )
--- pos/src/components/Orders.jsx
+++ pos/src/components/Orders.jsx
@@ -1,7 +1,7 @@
 export default function Orders() {
     return(
         <div className="orders-wrapper">
-
+            
         </div>
     );
 }
