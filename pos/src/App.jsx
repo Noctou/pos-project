6e4b1f3ef27fbd5a1a92601a4d7e8f285245cc63
Noctou <noctouu@gmail.com>
2025-08-09 18:16:12 +0800

    added sidebar

--- pos/src/App.jsx
+++ pos/src/App.jsx
@@ -1,21 +1,27 @@
-import ThemeToggle from "./components/ThemeToggle";
 import useTheme from './hooks/useThemeToggle';
+
+import ThemeToggle from "./components/ThemeToggle";
+import Sidebar from './components/Sidebar';
+import Checkout from './components/Checkout';
 
 import './App.css'
 
 function App() {
   const [darkMode, setDarkMode] = useTheme();
-  
+
   return (
-    <>
       <div className="container">
         <ThemeToggle darkMode={darkMode} toggleTheme={() => setDarkMode(!darkMode)} />
+        
+        <Sidebar />
           
         <div className="layer">
-          
+          <div className="orders-container">
+            <Checkout />
+          </div>
+
         </div>
       </div>
-    </>
   )
 }
 
