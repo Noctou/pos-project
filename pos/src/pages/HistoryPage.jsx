2b45a0b3ddcd44fb0d8d2f7facdcfad58a654602
Noctou <noctouu@gmail.com>
2025-08-14 01:17:56 +0800

    added styling to history page

--- pos/src/pages/HistoryPage.jsx
+++ pos/src/pages/HistoryPage.jsx
@@ -1,9 +1,11 @@
 export default function HistoryPage(){
     return(
-        <>
-            <div className="container history-page">
-                <h1>History Page</h1>
+        <div className="history-container">
+            {/* <div style={{ height: '12vh' }}></div> */}
+            <h1>Ordered Items History</h1>
+            <div className="purchase-history-wrapper">
+                {/* List of ordered items go here */}
             </div>
-        </>
-    )
+        </div>
+    );
 }
