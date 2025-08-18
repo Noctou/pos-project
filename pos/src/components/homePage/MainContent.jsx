92464c51c7a50c0b413e99e11a0f5c62aca782d2
Qutiepatutie <ralphandre8@gmail.com>
2025-08-18 22:44:32 +0800

    Added new items in stock

--- pos/src/components/homePage/MainContent.jsx
+++ pos/src/components/homePage/MainContent.jsx
@@ -6,16 +6,27 @@ import Dog from '../../assets/sticker-2.png';
 import Bird from '../../assets/sticker-3.png';
 import Cat from '../../assets/sticker-4.png';
 import Capybara from '../../assets/sticker-5.png';
+import MatchaHomie from '../../assets/sticker-6.png';
+import MatchaGirly from '../../assets/sticker-7.png';
+import Error from '../../assets/sticker-8.png';
+import Clover from '../../assets/sticker-9.png';
+import Chats from '../../assets/sticker-10.png';
+import Sparkle from '../../assets/sticker-11.png';
 
 /* Keychains */
-import EarlAgustin from '../../assets/track-1.png';
-import Ivos from '../../assets/track-2.png';
-import Niki from '../../assets/track-3.png';
-import Bea from '../../assets/track-4.png';
-import Clairo from '../../assets/track-5.png';
-import Faye from '../../assets/track-6.png';
-import Coj from '../../assets/track-7.png';
-import Tyler from '../../assets/track-8.png';
+import EarlAgustin from '../../assets/keychain-1.png';
+import Ivos from '../../assets/keychain-2.png';
+import Niki from '../../assets/keychain-3.png';
+import Bea from '../../assets/keychain-4.png';
+import Clairo from '../../assets/keychain-5.png';
+import Faye from '../../assets/keychain-6.png';
+import Coj from '../../assets/keychain-7.png';
+import Tyler from '../../assets/keychain-8.png';
+import Basketball from '../../assets/keychain-9-rect.png';
+import Airplane from '../../assets/keychain-10-rect.png';
+import OldHouse from '../../assets/keychain-11-rect.png';
+import Med from '../../assets/keychain-12-rect.png';
+import Heart from '../../assets/keychain-13-rect.png';
 
 export default function MainContent({ onAddToOrder, itemsFromDB, orderedItems }) {
     const [filter, setFilter] = useState("All");
@@ -29,6 +40,12 @@ export default function MainContent({ onAddToOrder, itemsFromDB, orderedItems })
         { name: "Loading Pigeon", src: Bird, category: "Sticker", price: stickerPrice },
         { name: "Catto Gaming", src: Cat, category: "Sticker", price: stickerPrice },
         { name: "Sleeping Capybara", src: Capybara, category: "Sticker", price: stickerPrice },
+        { name: "Matcha Homie", src: MatchaHomie, category: "Sticker", price: stickerPrice },
+        { name: "Matcha Girly", src: MatchaGirly, category: "Sticker", price: stickerPrice },
+        { name: "Error", src: Error, category: "Sticker", price: stickerPrice },
+        { name: "Clover", src: Clover, category: "Sticker", price: stickerPrice },
+        { name: "Chats", src: Chats, category: "Sticker", price: stickerPrice },
+        { name: "Sparkle", src: Sparkle, category: "Sticker", price: stickerPrice },
         { name: "Earl Agustin", src: EarlAgustin, category: "Keychain", price: keychainPrice },
         { name: "IV of Spades", src: Ivos, category: "Keychain", price: keychainPrice },
         { name: "Niki", src: Niki, category: "Keychain", price: keychainPrice },
@@ -37,6 +54,11 @@ export default function MainContent({ onAddToOrder, itemsFromDB, orderedItems })
         { name: "Faye Webster", src: Faye, category: "Keychain", price: keychainPrice },
         { name: "Tyler, The Creator", src: Tyler, category: "Keychain", price: keychainPrice },
         { name: "Cup of Joe", src: Coj, category: "Keychain", price: keychainPrice },
+        { name: "Basketball", src: Basketball, category: "rect", price: keychainPrice},
+        { name: "Airplane", src: Airplane, category: "rect", price: keychainPrice},
+        { name: "Old House", src: OldHouse, category: "rect", price: keychainPrice},
+        { name: "Med", src: Med, category: "rect", price: keychainPrice},
+        { name: "Heart", src: Heart, category: "rect", price: keychainPrice},
     ];
 
     const filteredItems = filter === "All" ? items : items.filter(item => item.category === filter);
@@ -82,7 +104,7 @@ export default function MainContent({ onAddToOrder, itemsFromDB, orderedItems })
                         onAddToOrder(item)}}
                     >
                     <img 
-                        className={item.category.toLowerCase()} 
+                        className={(item.category.toLowerCase().includes("rect")) ? "rect" : item.category.toLowerCase()} 
                         src={item.src} 
                         alt={item.name} 
                     />
